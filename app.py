573241d20d2299ebbff8923edb8b936e3e4f45a5
Kevin Chow <kevinchow360@gmail.com>
2025-11-23 20:51:29 -0500

    Enable CORS for frontend JS fetch

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
+from flask_cors import CORS
+CORS(app)
 
 # -----------------------
 # Config
